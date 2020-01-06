3a3129f709a101f53f61488528584b5895134df0
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-01-06 03:25:54 +0100

    bug fixed

--- fanInstall.sh
+++ fanInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # set current directory in fanSystem and copy file in /etc/init.
-sed "s|DIR=|$PWD|" fanSystem.sh > /etc/init.d/fanSystem.sh
+sed "s|DIR=|DIR=$PWD|" fanSystem.sh > /etc/init.d/fanSystem.sh
 
 # change folder's rights
 chmod u+x /etc/init.d/fanSystem.sh
